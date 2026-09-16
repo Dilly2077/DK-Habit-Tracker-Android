753b80d17ed110d3c552c5506abd98c8370e7d0d
Dilly2077 <khaznadar.dlovan@gmail.com>
2026-09-17 00:32:02 +0100

    Fix Compose KeyboardOptions resolution

---
+++ app/src/main/java/androidx/compose/ui/text/input/KeyboardOptionsCompat.kt
@@ -0,0 +1,6 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+package androidx.compose.ui.text.input
+
+/** Compatibility alias for Compose's KeyboardOptions location used by this project. */
+typealias KeyboardOptions = androidx.compose.foundation.text.KeyboardOptions
